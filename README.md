74ba0bd517bb38129fffc41ee33c82153ee35587
Jarret Lavallee <jarret.lavallee@puppet.com>
2017-03-27 09:40:00 -0700

    Skeleton puppet module files

---
+++ README.md
@@ -0,0 +1,16 @@
+[![Build Status](https://travis-ci.org/jarretlavallee/puppet-skel.svg?branch=master)](https://travis-ci.org/jarretlavallee/puppet-skel)
+
+# Skel Module
+
+
+## Preconfiguration
+
+
+## Feature 1
+
+## Use Case 
+
+## Use Case 2
+
+## Configuration
+
---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+# Class: skel
+# ===========================
+#
+# Description of the module
+#
+# Parameters
+# ----------
+#
+# Variables
+# ----------
+#
+#
+# Examples
+# --------
+#
+# @example
+#
+# Authors
+# -------
+#
+# Jarret Lavallee <jarret.lavallee@puppet.com>
+#
+# Copyright
+# ---------
+#
+# Copyright 2017 Jarret Lavallee, unless otherwise noted.
+#
+class skel () {
+
+}
